eeafe762294a8169889f992c6cab806ec00db875
andria.gv <gvaramiaandria1@gmail.com>
2024-10-25 13:49:26 +0400

    first commit

---
+++ CollabProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CollabProject
+//
+//  Created by Apple on 25.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
