c5d90fdc8c7dcbd0f5b38e6ddc162830b53ce8c9
irakli <46853093+iraklijanashvili@users.noreply.github.com>
2024-10-26 17:47:50 +0400

    Create FacoritePageCollectionViewCell.swift
    create uiComponents

--- CollabProject/Scenes/FavoritePage/Cell/FacoritePageCollectionViewCell.swift
+++ CollabProject/Scenes/FavoritePage/Cell/FacoritePageCollectionViewCell.swift
@@ -4,9 +4,84 @@
 //
 //  Created by Apple on 25.10.24.
 //
-
+ 
 import UIKit
-
-class FacoritePageCollectionViewCell: UICollectionViewCell {
+ 
+class FavoritePageCollectionViewCell: UICollectionViewCell {
     
+    private let posterImageView: UIImageView = {
+            let imageView = UIImageView()
+        imageView.contentMode =  .scaleAspectFill
+            imageView.clipsToBounds = true
+            imageView.layer.cornerRadius = 8
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            return imageView
+        }()
+ 
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16, weight: .medium)
+        label.textAlignment = .left
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let starImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Star")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let ratingLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .gray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        contentView.addSubview(posterImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(starImageView)
+        contentView.addSubview(ratingLabel)
+        
+        NSLayoutConstraint.activate([
+            posterImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            posterImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            posterImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            posterImageView.heightAnchor.constraint(equalToConstant: 230),
+            
+            
+            titleLabel.topAnchor.constraint(equalTo: posterImageView.bottomAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            starImageView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
+            starImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            starImageView.widthAnchor.constraint(equalToConstant: 16),
+            starImageView.heightAnchor.constraint(equalToConstant: 16),
+            
+            ratingLabel.centerYAnchor.constraint(equalTo: starImageView.centerYAnchor),
+            ratingLabel.leadingAnchor.constraint(equalTo: starImageView.trailingAnchor, constant: 4),
+            ratingLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
+    }
+ 
+    func configure(with movie: Movie) {
+        posterImageView.image = movie.movieImage
+        titleLabel.text = movie.name
+        ratingLabel.text = "\(movie.imdbRating) IMDb"
+    }
 }
+
