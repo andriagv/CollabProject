68e8348836d476f65e04ff8068122c42916b07ed
TinaTinaJ <jishkarianitiko@gmail.com>
2024-10-27 18:48:19 +0400

    Update (Typo) SceneDelegate.swift
    Typo in scene

--- CollabProject/App/SceneDelegate.swift
+++ CollabProject/App/SceneDelegate.swift
@@ -28,7 +28,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let scene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: scene)
-        let vc = tabBarController()
+        let vc = TabBarController()
         let navCon = UINavigationController(rootViewController: vc)
         vc.view.backgroundColor = .white
         
