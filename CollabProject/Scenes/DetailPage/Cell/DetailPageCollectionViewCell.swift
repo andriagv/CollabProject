cd397486c282d01f257ddacf3b581f6e311b28f5
Levan Gorjeladze <62937866+levangorjeladze@users.noreply.github.com>
2024-10-27 14:28:39 +0400

    Update DetailPageCollectionViewCell.swift

--- CollabProject/Scenes/DetailPage/Cell/DetailPageCollectionViewCell.swift
+++ CollabProject/Scenes/DetailPage/Cell/DetailPageCollectionViewCell.swift
@@ -5,9 +5,32 @@
 //  Created by Apple on 25.10.24.
 //
 
-import UIKit
-
-class DetailPageCollectionViewCell: UICollectionViewCell {
+import Foundation
+ 
+protocol DetailPageViewModelDelegate: AnyObject {
+    func navigateBackToRoot()
+}
+ 
+final class DetailPageViewModel {
+    
+    let vc = MovieList()
+    var Allmovies: [Movie]
+    weak var delegate: DetailPageViewModelDelegate?
     
     
+    init() {
+        Allmovies = vc.allMovies
+    }
+    
+    func numberOfItems() -> Int {
+        Allmovies.count
+    }
+    
+    func movie(at index: Int) -> Movie {
+        return Allmovies[index]
+    }
+    
+    func backButtonTapped() {
+        delegate?.navigateBackToRoot()
+    }
 }
