b18c5163e95bb0e62f4f85b3b56bb5814bbb18d9
Levan Gorjeladze <62937866+levangorjeladze@users.noreply.github.com>
2024-10-27 14:37:53 +0400

    Update DetailPageCollectionViewCell.swift
    update

--- CollabProject/Scenes/DetailPage/Cell/DetailPageCollectionViewCell.swift
+++ CollabProject/Scenes/DetailPage/Cell/DetailPageCollectionViewCell.swift
@@ -5,32 +5,53 @@
 //  Created by Apple on 25.10.24.
 //
 
-import Foundation
+import UIKit
  
-protocol DetailPageViewModelDelegate: AnyObject {
-    func navigateBackToRoot()
-}
- 
-final class DetailPageViewModel {
+class DetailPageCollectionViewCell: UICollectionViewCell {
     
-    let vc = MovieList()
-    var Allmovies: [Movie]
-    weak var delegate: DetailPageViewModelDelegate?
+    static var identifier = "DetailPageCollectionViewCell"
     
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.textAlignment = .left
+        label.numberOfLines = 0
+        return label
+    }()
     
-    init() {
-        Allmovies = vc.allMovies
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = 10
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(imageView)
+        contentView.backgroundColor = .clear //change
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 78),
+            imageView.heightAnchor.constraint(equalToConstant: 78),
+            nameLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 7),
+            nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
+        ])
     }
     
-    func numberOfItems() -> Int {
-        Allmovies.count
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
-    func movie(at index: Int) -> Movie {
-        return Allmovies[index]
-    }
-    
-    func backButtonTapped() {
-        delegate?.navigateBackToRoot()
-    }
+    func configure(with cast: Movie.Cast) {
+        nameLabel.text = cast.name
+        imageView.image = cast.image
+    }    
 }
