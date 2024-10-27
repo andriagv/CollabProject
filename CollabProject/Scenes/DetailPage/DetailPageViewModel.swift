6a2044b53ecb732f2adbf939ae02463d8da19fb9
Levan Gorjeladze <62937866+levangorjeladze@users.noreply.github.com>
2024-10-27 14:27:56 +0400

    Update DetailPageViewModel.swift

--- CollabProject/Scenes/DetailPage/DetailPageViewModel.swift
+++ CollabProject/Scenes/DetailPage/DetailPageViewModel.swift
@@ -6,8 +6,31 @@
 //
 
 import Foundation
-
-
+ 
+protocol DetailPageViewModelDelegate: AnyObject {
+    func navigateBackToRoot()
+}
+ 
 final class DetailPageViewModel {
     
+    let vc = MovieList()
+    var Allmovies: [Movie]
+    weak var delegate: DetailPageViewModelDelegate?
+    
+    
+    init() {
+        Allmovies = vc.allMovies
+    }
+    
+    func numberOfItems() -> Int {
+        Allmovies.count
+    }
+    
+    func movie(at index: Int) -> Movie {
+        return Allmovies[index]
+    }
+    
+    func backButtonTapped() {
+        delegate?.navigateBackToRoot()
+    }
 }
